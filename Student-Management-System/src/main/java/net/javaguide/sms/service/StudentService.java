8a8fbd2a5482a514251beefe9cfd229ff7868675
User <abeedshaik997@gamil.com>
2022-12-03 14:26:06 +0530

    first commit

---
+++ Student-Management-System/src/main/java/net/javaguide/sms/service/StudentService.java
@@ -0,0 +1,19 @@
+package net.javaguide.sms.service;
+
+import java.util.List;
+
+import net.javaguide.sms.entity.Student;
+
+public interface StudentService {
+	
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudent(Long id);
+
+}
